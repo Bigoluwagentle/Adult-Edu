034440d6a3dbdce04e080f7d38292d78ee9b6b86
bigoluwagentle <olayiwolaolabode06@gmail.com>
2025-10-30 13:52:26 -0700

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -5,3 +5,20 @@ const nextConfig = {
 };
 
 export default nextConfig;
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBv8yL6HJbXuVZ39gPKadx2GvzN0eMeKa4",
+//   authDomain: "adult-edu.firebaseapp.com",
+//   projectId: "adult-edu",
+//   storageBucket: "adult-edu.firebasestorage.app",
+//   messagingSenderId: "75602150291",
+//   appId: "1:75602150291:web:92198ce3700570e3025e66"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
